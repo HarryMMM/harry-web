fee83f92672458250861fa8f279c5c31017da758
harry <hhr_harry@qq.com>
2019-05-26 22:16:09 +0800

    test show svg

---
+++ src/main/java/cn/harryai/harryweb/HarryWebApplication.java
@@ -0,0 +1,13 @@
+package cn.harryai.harryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarryWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarryWebApplication.class, args);
+    }
+
+}
